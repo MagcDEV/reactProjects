0924da2aaf36a9d7c9a51a06f330a18cab657623
Manuel Guevara <manuel117g@gmail.com>
2021-02-17 16:34:26 -0400

    Add files via upload

---
+++ fecthapi/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import './index.css';
+
+const url = 'https://api.github.com/users';
+
+const UseEffectFetchData = () => {
+  return <h2>fetch data</h2>;
+};
+
+export default UseEffectFetchData;
