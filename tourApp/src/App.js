87615af19f1a306c504f21713059279a3496506e
Manuel Guevara <manuel117g@gmail.com>
2021-02-22 13:40:47 -0400

    Add files via upload

--- tourApp/src/App.js
+++ tourApp/src/App.js
@@ -4,11 +4,33 @@ import Tours from './Tours'
 import './index.css';
 import Loading from './Loading';
 
-const url = 'http://course-api.netlify.app/api/react-tours-projects';
+const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
     const [loading, setLoading] = useState(true)
     const [tours, setTours] = useState([])
+
+    const deleteTour = (id) => {
+        return setTours(tours.filter((tour) => tour.id !== id))
+    }
+
+    const fetchTours = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(url);
+            const tours = await response.json();
+            setLoading(false);
+            setTours(tours);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchTours();
+    }, [])
+
     if (loading) {
         return (
             <main>
@@ -16,9 +38,19 @@ const App = () => {
             </main>
         );
     }
+    if(tours.length === 0) {
+        return (
+            <main>
+                <div className='title'>
+                    <h2>no tours left</h2>
+                    <button className='btn' onClick={fetchTours}>refresh</button>
+                </div>
+            </main>
+        )
+    }
     return (
             <main>
-                <Tours />
+                <Tours tours={tours} deleteTour={deleteTour}/>
             </main>
     );
 }
